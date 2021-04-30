c33f74eb76d61e0eaaeba03833be04882f8f1ff2
Espen Norderud <espen42@gmail.com>
2021-04-30 16:02:31 +0200

    #160: Parse SSR engine settings from react4xp (depends on react4xp-npm 1.4)

--- src/main/java/com/enonic/lib/react4xp/ssr/ServerSideRenderer.java
+++ src/main/java/com/enonic/lib/react4xp/ssr/ServerSideRenderer.java
@@ -48,12 +48,11 @@ public class ServerSideRenderer implements ScriptBean {
     private NashornScriptEngine ENGINE = null;
     private Supplier<ResourceService> RESOURCE_SERVICE_SUPPLIER;
 
-    //private String processedCode = "";
-    //private int loggedLines = 0;
 
 
+    ////////////////////////////////////////////////////////////////////////// INIT
 
-    public void setConfig(
+    public void setup(
             String APP_NAME,
             String SCRIPTS_HOME,
             String LIBRARY_NAME,
@@ -100,31 +99,14 @@ public class ServerSideRenderer implements ScriptBean {
                                                                                                                         }
                 loadAssets(dependencies);
             }
-            LOG.info("##################### /ServerSideRenderer" + this.hashCode() + ".setconfig done.");
+                                                                                                                        LOG.info("##################### /ServerSideRenderer" + this.hashCode() + ".setconfig done.");
         }
     }
 
-    ///////////////////////////////////////////////////////////////
 
 
-    /*private void addEntriesAndChunksScripts(String CHUNKFILES_HOME, String COMPONENT_STATS_FILENAME, ArrayList<String> CHUNK_SOURCEFILENAMES, String ENTRIES_SOURCEFILENAME, LinkedList<String> scriptExecutionOrder, HashMap<String, String> scriptContentsByFilename, boolean doLazyLoad) throws IOException {
-        String ENTRIES_SOURCE = CHUNKFILES_HOME + ENTRIES_SOURCEFILENAME;
-        ArrayList<String> CHUNKS_SOURCES = new ArrayList<>();
-        for (String chunkFileName : CHUNK_SOURCEFILENAMES) {
-            CHUNKS_SOURCES.add(CHUNKFILES_HOME + chunkFileName);
-        }
-        LOG.info("initBasicSettings - CHUNKS_SOURCES: " + CHUNKS_SOURCES);
 
-        LinkedHashSet<String> transpiledDependencies = new ChunkDependencyParser().getScriptDependencyNames(CHUNKFILES_HOME + COMPONENT_STATS_FILENAME, CHUNKS_SOURCES, ENTRIES_SOURCE, doLazyLoad);
-
-        for (String scriptFile : transpiledDependencies) {
-            String file = CHUNKFILES_HOME + scriptFile;
-            scriptContentsByFilename.put(file, ResourceHandler.readResource(file));
-            scriptExecutionOrder.add(file);
-            scriptHasBeenLoadedByName.put(file, false);
-            LOG.info("addEntriesAndChunksScripts - scriptHasBeenLoadedByName: " + file + " -> false");
-        }
-    }*/
+    /////////////////////////////////////////////////////////////// Helpers
 
 
     private void ensureProdCache(List<String> assetNames) {
@@ -140,6 +122,38 @@ public class ServerSideRenderer implements ScriptBean {
         }
     }
 
+
+
+    private LinkedList<String> getRunnableAssetNames(String entryName, String dependencyNames) {
+        LinkedList<String> runnableAssets = new LinkedList<>();
+
+        if (dependencyNames != null && !"".equals(dependencyNames.trim())) {
+            JSONArray array = new JSONArray(dependencyNames);
+            Iterator<Object> it = array.iterator();
+            while (it.hasNext()) {
+                String assetName = (String)it.next();
+                if (assetName.endsWith(".js")) {
+                    runnableAssets.add(assetName);
+                }
+            }
+        }
+        String fullEntryName = entryName + ".js";
+        runnableAssets.add(fullEntryName);
+
+        return runnableAssets;
+    }
+
+
+
+    private void loadAssets(LinkedList<String> runnableAssets) {
+        ensureProdCache(runnableAssets);
+        for (String assetName : runnableAssets) {
+            maybeLoadAsset(assetName);
+        }
+    }
+
+
+
     private void maybeLoadAsset(String assetName) {
         if (IS_PRODMODE) {
             CacheMarker assetCacheMarker = ASSET_CACHE_MARKERS.get(assetName);
@@ -153,28 +167,6 @@ public class ServerSideRenderer implements ScriptBean {
         }
     }
 
-/*
-    private void dumpPrettyProcessedCode() {
-        if (!"".equals(processedCode)) {
-            String[] lines = processedCode.split("[\\n\\r]");
-            int newLines = lines.length + loggedLines;
-            StringBuilder code = new StringBuilder();
-            String formatString = "%" + ((int)(Math.floor(Math.log10(newLines))) + 1) +"d |    %s\n";
-            for (String line : lines) {
-                code.append(String.format(formatString, ++loggedLines, line));
-            }
-            String pretty = code.toString();
-
-            LOG.info("\nIncremental code dump (all code processed in the Nashorn SSR engine, and unlogged until now - so earlier code may be found above):\n" +
-                    "---------------------------------\n\n" +
-                    pretty + "\n\n" +
-                    "---------------------------------------\n");
-
-            // In case of repeated errors, empty this after logging to prevent excessive flooding in the log (the line numbers will keep counting though):
-            processedCode = "";
-        }
-    }
-    */
 
 
     /** Load both entry assets and JS dependency chunks into the Nashorn engine */
@@ -195,7 +187,7 @@ public class ServerSideRenderer implements ScriptBean {
 
             if (IS_PRODMODE) {
                 ASSET_CACHE_MARKERS.get(assetName).isCached = true;
-                LOG.info("Loaded and cached in Nashorn engine: " + assetName);
+                                                                                                                        LOG.info("Loaded and cached in Nashorn engine: " + assetName);
             }
 
         } catch (IOException e1) {
@@ -232,26 +224,6 @@ public class ServerSideRenderer implements ScriptBean {
         }
     }
 
-    private String getCleanErrorMessage(Exception e) {
-        return e.getMessage().replaceAll(" in <eval> at line number \\d+ at column number \\d++", "");
-    }
-
-    private String getSolutionTips() {
-        return "\nSOLUTION TIPS: The previous error message may refer to lines in compiled/mangled code. To increase readability, you can try react4xp clientside-rendering or building react4xp with buildEnv = development or gradle CLI argument -Pdev. Remember to clear all cached behavior first (stop continuous builds, clear/rebuild your project, restart the XP server, clear browser cache). Actual line numbers in compiled JS source files tends to be referred in the stack trace above: look for '(<eval>: [lineNumber])' on the lines below '...NativeError.java...':.\n\n";
-    }
-
-    private class RenderException extends RuntimeException {
-        private final String message;
-
-        private RenderException(String message) {
-            this.message = message;
-        }
-
-        @Override
-        public String getMessage() {
-            return message;
-        }
-    }
 
     private Map<String, String> runSSR(String entry, String props, LinkedList<String> assetsInvolved) throws ScriptException {
 
@@ -283,7 +255,7 @@ public class ServerSideRenderer implements ScriptBean {
             if (IS_PRODMODE && assetsInvolved != null) {
                                                                                                                         LOG.info("runSSR - assetsInvolved: " + assetsInvolved);
                 for (String asset : assetsInvolved) {
-                                                                                                                            LOG.info("runSSR - removing asset cache: " + asset);
+                                                                                                                        LOG.info("runSSR - removing asset cache: " + asset);
                     CacheMarker cacheMarker = ASSET_CACHE_MARKERS.get(asset);
                     synchronized (cacheMarker) {
                         cacheMarker.isCached = false;
@@ -300,33 +272,12 @@ public class ServerSideRenderer implements ScriptBean {
 
 
 
-    private LinkedList<String> getRunnableAssetNames(String entryName, String dependencyNames) {
-        LinkedList<String> runnableAssets = new LinkedList<>();
 
-        if (dependencyNames != null && !"".equals(dependencyNames.trim())) {
-            JSONArray array = new JSONArray(dependencyNames);
-            Iterator<Object> it = array.iterator();
-            while (it.hasNext()) {
-                String assetName = (String)it.next();
-                if (assetName.endsWith(".js")) {
-                    runnableAssets.add(assetName);
-                }
-            }
-        }
-        String fullEntryName = entryName + ".js";
-        runnableAssets.add(fullEntryName);
+///////////////////////////////////////////////////////////// Error handling
 
-        return runnableAssets;
+    private String getCleanErrorMessage(Exception e) {
+        return e.getMessage().replaceAll(" in <eval> at line number \\d+ at column number \\d++", "");
     }
-
-
-    private void loadAssets(LinkedList<String> runnableAssets) {
-        ensureProdCache(runnableAssets);
-        for (String assetName : runnableAssets) {
-            maybeLoadAsset(assetName);
-        }
-    }
-
 
     private String getLoggableStackTrace(Exception e, String overrideMessage) {
         String message = (overrideMessage == null)
@@ -337,63 +288,45 @@ public class ServerSideRenderer implements ScriptBean {
         return e.getClass().getName() + ": " + message + "\n" + sw;
     }
 
+    private String getSolutionTips() {
+        return "\nSOLUTION TIPS: The previous error message may refer to lines in compiled/mangled code. To increase readability, you can try react4xp clientside-rendering or building react4xp with buildEnv = development or gradle CLI argument -Pdev. Remember to clear all cached behavior first (stop continuous builds, clear/rebuild your project, restart the XP server, clear browser cache). Actual line numbers in compiled JS source files tends to be referred in the stack trace above: look for '(<eval>: [lineNumber])' on the lines below '...NativeError.java...':.\n\n";
+    }
 
-    ///////////////////////////////////////////////////////
+    private class RenderException extends RuntimeException {
+        private final String message;
 
-    /**
-     * Renders an entry to an HTML string.
-     * @param entryName name of a transpiled JSX component, i.e. jsxPath: the filextension-less path to the compiled entry asset under assets/react4xp/, e.g: "site/parts/simple-reactive/simple-reactive"
-     * @param props valid stringified JSON object: the entry's react props, e.g. '{"insertedMessage": "this is a prop!"}'
-     * @returns {Map} Under the key 'html' (KEY_HTML), naked rendered HTML if successful. Under the key 'error' (KEY_ERROR), error message if failed.
-     */
-    public Map<String, String> render(String entryName, String props) {
+        private RenderException(String message) {
+            this.message = message;
+        }
 
-                                                                                                                                LOG.info("--------------------------------------- render - START:\n" +
-                                                                                                                                        "    render - Thread ID: " + Thread.currentThread().getId() + "\n" +
-                                                                                                                                        "    render - ServerSideRenderer" + this.hashCode() + "\n" +
-                                                                                                                                        "    render - Engine" + ENGINE.hashCode() + "\n" +
-                                                                                                                                        "    render - entryName: " + entryName);
-
-        try {
-            LinkedList<String> runnableAssetNames = getRunnableAssetNames(entryName, null);
-            loadAssets(runnableAssetNames);
-            Map<String, String> rendered = runSSR(entryName, props, runnableAssetNames);
-                                                                                                                        LOG.info("Rendered:\n    " + KEY_HTML + ": " + rendered.get(KEY_HTML) + "\n    " + KEY_ERROR + ": " + rendered.get(KEY_ERROR));
-                                                                                                                        LOG.info("---------------------- render (" +  entryName + ") - the end.\n\n\n\n");
-            return rendered;
-
-        } catch (RenderException r) {
-                                                                                                                        LOG.info("---------------------- render (" +  entryName + ") - I caught something.\n\n\n\n");
-            return Map.of(
-                    KEY_ERROR, r.getMessage()
-            );
-
-        } catch (Exception e) {
-            LOG.error(getLoggableStackTrace(e, null));
-                                                                                                                        LOG.info("---------------------- render (" +  entryName + ") - I dieded.\n\n\n\n");
-            return Map.of(
-                    KEY_ERROR, e.getClass().getName() + ": " + e.getMessage()
-            );
+        @Override
+        public String getMessage() {
+            return message;
         }
     }
 
 
 
+
+////////////////////////////////////////////////////////////////  Render
+
+
+
     /**
-     * Same as renderToString, but only used when the engine has been initialized (setConfig) with lazyLoading = true
+     * Renders an entry to an HTML string.
      * @param entryName name of a transpiled JSX component, i.e. jsxPath: the filextension-less path to the compiled entry asset under assets/react4xp/, e.g: "site/parts/simple-reactive/simple-reactive"
      * @param props valid stringified JSON object: the entry's react props, e.g. '{"insertedMessage": "this is a prop!"}'
-     * @param dependencyNames valid stringified JSON array: a set of file names to lazy-load into the engine, needed by the entry before running it
+     * @param dependencyNames valid stringified JSON array: a set of file names to load into the engine (if not already done during initialization), needed by the entry before running it
      * @returns {Map} Under the key 'html' (KEY_HTML), naked rendered HTML if successful. Under the key 'error' (KEY_ERROR), error message if failed.
      */
-    public Map<String, String> renderLazy(String entryName, String props, String dependencyNames) {
+    public Map<String, String> render(String entryName, String props, String dependencyNames) {
 
-                                                                                                                                        LOG.info("--------------------------------------- renderLazy - START:\n" +
-                                                                                                                                                "    renderLazy - Thread ID: " + Thread.currentThread().getId() + "\n" +
-                                                                                                                                                "    renderLazy - ServerSideRenderer" + this.hashCode() + "\n" +
-                                                                                                                                                "    renderLazy - Engine" + ENGINE.hashCode() + "\n" +
-                                                                                                                                                "    renderLazy - entryName: " + entryName + "\n" +
-                                                                                                                                                "    renderLazy - dependencyNames: " + dependencyNames);
+                                                                                                                        LOG.info("--------------------------------------- renderLazy - START:\n" +
+                                                                                                                                "    renderLazy - Thread ID: " + Thread.currentThread().getId() + "\n" +
+                                                                                                                                "    renderLazy - ServerSideRenderer" + this.hashCode() + "\n" +
+                                                                                                                                "    renderLazy - Engine" + ENGINE.hashCode() + "\n" +
+                                                                                                                                "    renderLazy - entryName: " + entryName + "\n" +
+                                                                                                                                "    renderLazy - dependencyNames: " + dependencyNames);
 
         try {
             LinkedList<String> runnableAssetNames = getRunnableAssetNames(entryName, dependencyNames);
