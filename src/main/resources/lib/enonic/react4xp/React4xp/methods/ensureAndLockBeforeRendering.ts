19d37cb60d34c8cf3e1952ea026655c2696941f7
Christian Westgaard <comlock@gmail.com>
2022-03-11 14:25:20 +0100

    Debug

--- src/main/resources/lib/enonic/react4xp/React4xp/methods/ensureAndLockBeforeRendering.ts
+++ src/main/resources/lib/enonic/react4xp/React4xp/methods/ensureAndLockBeforeRendering.ts
@@ -1,9 +1,15 @@
+//import {toStr} from '@enonic/js-utils/value/toStr';
+
+
 export function ensureAndLockBeforeRendering() {
 	this.ensureAndLockId();
 
+	//log.debug('ensureAndLockBeforeRendering() this.jsxPath:%s', toStr(this.jsxPath));
 	if (!this.jsxPath) {
 		throw Error("Target path for JSX component, jsxPath, has not been set. Add an absolute path (or an XP component from which to derive it) in the React4XP constructor or with the setters.");
 	}
+
+	//log.debug('ensureAndLockBeforeRendering() this.react4xpId:%s', toStr(this.react4xpId));
 	if (!this.react4xpId) {
 		throw Error("ID for the target container element is missing.");
 	}
