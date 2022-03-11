a8cb8efe24f304b770ea9f7a4e5e76a921bb29d7
Christian Westgaard <comlock@gmail.com>
2022-03-11 14:25:20 +0100

    Debug

--- src/main/resources/lib/enonic/react4xp/React4xp/methods/ensureAndLockId.ts
+++ src/main/resources/lib/enonic/react4xp/React4xp/methods/ensureAndLockId.ts
@@ -1,12 +1,18 @@
+//import {toStr} from '@enonic/js-utils/value/toStr';
+
+
 // For now, it seems like a good idea to ensure two things when starting the client side rendering:
 // 1, there is a target ID set.
 // 2, it can't be changed once the rendering has started, i.e. between render body and render pagecontributions
 export function ensureAndLockId() {
+	//log.debug('ensureAndLockId() this.react4xpId:%s', toStr(this.react4xpId));
 	if (!this.react4xpId) {
 		this.uniqueId();
 	}
+	//log.debug('ensureAndLockId() this.react4xpIdIsLocked:%s', toStr(this.react4xpIdIsLocked));
 	this.react4xpIdIsLocked = true;
 	if (this.react4xpId && this.props) {
 		this.props.react4xpId = this.react4xpId;
 	}
+	//log.debug('ensureAndLockId() this.props:%s', toStr(this.props));
 }
--- src/main/resources/lib/enonic/react4xp/React4xp/methods/setId.ts
+++ src/main/resources/lib/enonic/react4xp/React4xp/methods/setId.ts
@@ -1,15 +1,22 @@
 import type {React4xp as React4xpNamespace} from '../../../../../index.d';
 
 
+//import {toStr} from '@enonic/js-utils/value/toStr';
+
+
 /** Sets the react4xpId - the HTML ID of the target container this component will be rendered into.
  * Deletes the ID if argument is omitted.
  * @returns The react4xp component itself, for builder-like pattern.
  */
 export function setId(react4xpId :React4xpNamespace.Id) {
+	//log.debug('setId() react4xpId:%s', toStr(react4xpId));
 	this.checkIdLock();
 	this.react4xpId = react4xpId;
+
+	//log.debug('setId() before this.props:%s', toStr(this.props));
 	if (this.props) {
 		this.props.react4xpId = react4xpId;
 	}
+	//log.debug('setId() after this.props:%s', toStr(this.props));
 	return this;
 }
