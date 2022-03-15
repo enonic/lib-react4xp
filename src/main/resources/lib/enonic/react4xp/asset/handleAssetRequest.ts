fa483286d1ad46b8a8eea73742fd5425ee7f5a03
Christian Westgaard <comlock@gmail.com>
2022-03-15 10:40:10 +0100

    getImmuteables()

--- src/main/resources/lib/enonic/react4xp/asset/handleAssetRequest.ts
+++ src/main/resources/lib/enonic/react4xp/asset/handleAssetRequest.ts
@@ -9,27 +9,14 @@ import {toStr} from '@enonic/js-utils/value/toStr';
 //import {getClientUrls} from '/lib/enonic/react4xp/dependencies';
 
 import {eTagGetter} from './eTagGetter';
-import {getDependencies} from './getDependencies';
+import {getImmuteables} from './getImmuteables';
 import {getEntries} from './getEntries';
-import {getExternals} from './getExternals';
 import {immuteableGetter} from './immuteableGetter';
 
 
 const ENTRIES = getEntries();
 //log.debug('handleAssetRequest ENTRIES:%s', toStr(ENTRIES));
-const dependencies = getDependencies(ENTRIES);
-const externals = getExternals();
-//log.debug('handleAssetRequest externals:%s', toStr(externals));
-
-const IMMUTEABLES = {};
-for (let i = 0; i < dependencies.length; i++) {
-	const dependency = dependencies[i];
-	IMMUTEABLES[dependency] = true;
-}
-for (let i = 0; i < externals.length; i++) {
-	const external = externals[i];
-	IMMUTEABLES[external] = true;
-}
+const IMMUTEABLES = getImmuteables(ENTRIES);
 //log.debug('handleAssetRequest IMMUTEABLES:%s', toStr(IMMUTEABLES));
 
 
