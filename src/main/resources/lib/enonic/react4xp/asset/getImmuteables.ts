1d2b4bd94f1db9ccb8f2481399e7db6e90a23c56
Christian Westgaard <comlock@gmail.com>
2022-03-15 10:40:10 +0100

    getImmuteables()

---
+++ src/main/resources/lib/enonic/react4xp/asset/getImmuteables.ts
@@ -0,0 +1,20 @@
+import {getDependencies} from './getDependencies';
+import {getExternals} from './getExternals';
+
+
+export function getImmuteables(entries :Array<string>) {
+	const dependencies = getDependencies(entries);
+	const externals = getExternals();
+	//log.debug('handleAssetRequest externals:%s', toStr(externals));
+
+	const immuteables = {};
+	for (let i = 0; i < dependencies.length; i++) {
+		const dependency = dependencies[i];
+		immuteables[dependency] = true;
+	}
+	for (let i = 0; i < externals.length; i++) {
+		const external = externals[i];
+		immuteables[external] = true;
+	}
+	return immuteables;
+}
