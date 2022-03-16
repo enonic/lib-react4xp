06ec864b25fd2f3ac740f250be6022c2b6c93c90
Christian Westgaard <comlock@gmail.com>
2022-03-16 10:45:30 +0100

    cacheKnownAssets()

--- src/main/resources/lib/enonic/react4xp/asset/immuteableGetter.ts
+++ src/main/resources/lib/enonic/react4xp/asset/immuteableGetter.ts
@@ -11,5 +11,9 @@ import {buildGetter} from '/lib/enonic/static';
 
 export const immuteableGetter = buildGetter({
 	etag: false, // default is true in production and false in development
+	getCleanPath: (request :Request) => {
+        const prefix = request.contextPath;
+		return prefix ? request.rawPath.substring(prefix.length) : request.rawPath;
+    },
 	root: R4X_TARGETSUBDIR // assets/react4xp
 }) as (request: Request) => Response;
