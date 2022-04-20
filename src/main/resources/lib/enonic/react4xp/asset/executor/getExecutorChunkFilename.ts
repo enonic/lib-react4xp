42e93651fc6a50e15d6e2ec53c0e8799f7be0e9d
Christian Westgaard <comlock@gmail.com>
2022-04-20 13:55:51 +0200

    Comment out debug

--- src/main/resources/lib/enonic/react4xp/asset/executor/getExecutorChunkFilename.ts
+++ src/main/resources/lib/enonic/react4xp/asset/executor/getExecutorChunkFilename.ts
@@ -1,4 +1,4 @@
-import {toStr} from '@enonic/js-utils/value/toStr';
+//import {toStr} from '@enonic/js-utils/value/toStr';
 import {RESOURCE_PATH_ABSOLUTE_EXECUTOR_MANIFEST_JSON} from '/lib/enonic/react4xp/constants';
 import {getResource} from '/lib/enonic/react4xp/resource/getResource';
 //@ts-ignore
@@ -28,7 +28,7 @@ export function getExecutorChunkFilename() {
         log.error(e.message);
         throw new Error(`Error when readText(${RESOURCE_PATH_ABSOLUTE_EXECUTOR_MANIFEST_JSON})!`);
     }
-	log.debug('getExecutorChunkFilename() content:%s', toStr(content));
+	//log.debug('getExecutorChunkFilename() content:%s', toStr(content));
 
 	let executorManifest :{
 		'executor.js' :string
@@ -36,7 +36,7 @@ export function getExecutorChunkFilename() {
 	try {
 		executorManifest = JSON.parse(content);
 		CACHED_EXECUTOR_CHUNK_FILE_NAME = executorManifest['executor.js'];
-		log.debug('getExecutorChunkFilename() CACHED_EXECUTOR_CHUNK_FILE_NAME:%s', toStr(CACHED_EXECUTOR_CHUNK_FILE_NAME));
+		//log.debug('getExecutorChunkFilename() CACHED_EXECUTOR_CHUNK_FILE_NAME:%s', toStr(CACHED_EXECUTOR_CHUNK_FILE_NAME));
 		return CACHED_EXECUTOR_CHUNK_FILE_NAME;
     } catch (e) {
         log.error(e.message);
