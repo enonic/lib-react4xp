2598d7ce7fe134bbd7ae1102006ab5cc8f6e7ca0
Christian Westgaard <comlock@gmail.com>
2022-04-26 15:13:02 +0200

    Remove unused file

--- src/main/resources/lib/enonic/react4xp/dependencies/getServiceRoot.ts
+++
@@ -1,6 +0,0 @@
-import {initServiceUrlRoot} from '/lib/enonic/react4xp/dependencies/initServiceUrlRoot';
-
-
-export function getServiceRoot(serviceName :string = '') {
-    return initServiceUrlRoot(serviceName);
-};
