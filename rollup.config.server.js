ec1abba850d5a91e2997bb5043d4dcac31d70a13
Christian Westgaard <comlock@gmail.com>
2022-04-27 12:29:57 +0200

    Fix build

--- rollup.config.server.js
+++ rollup.config.server.js
@@ -3,6 +3,11 @@ import commonjs from '@rollup/plugin-commonjs';
 //import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
+
+const DIR_IN_REL = 'src/main/resources';
+const DIR_OUT_REL = 'build/resources/main';
+
+
 export default {
 	external: [
 		'/lib/cache',
@@ -27,7 +32,7 @@ export default {
 	output: {
 		chunkFileNames: '[name].js',
 
-		dir: 'build/resources/main',
+		dir: DIR_OUT_REL,
 
 		// (!) Entry module "node_modules/@enonic/js-utils/dist/cjs/index.js"
 		// is implicitly using "default" export mode, which means for CommonJS
@@ -52,25 +57,17 @@ export default {
 		interop: false,
 		//makeAbsoluteExternalsRelative: false, // Only works on CLI!
 		preserveModules: true, // Copy modules into build/resources/main/node_modules instead of bundling them
-		preserveModulesRoot: 'src/main/resources',
+		preserveModulesRoot: DIR_IN_REL,
 		sourcemap: false
 	},
 	plugins: [
 		alias({
 			entries: [
-				{ find: '@enonic/react4xp', replacement: 'node_modules/@enonic/react4xp/dist' },
-				//{ find: '@enonic/js-utils', replacement: 'node_modules/@enonic/js-utils/src' } // [!] Error: Unexpected token (Note that you need plugins to import files that are not JavaScript)
-				{ find: '@enonic/js-utils', replacement: 'node_modules/@enonic/js-utils/dist/cjs' }
-				//{ find: '@enonic/js-utils', replacement: 'node_modules/@enonic/js-utils/dist/esm' } // [!] Error: Could not load node_modules/@enonic/js-utils/dist/esm (imported by src/main/resources/lib/enonic/react4xp/pageContributions.ts): EISDIR: illegal operation on a directory, read
+				{ find: '@enonic/react4xp', replacement: 'node_modules/@enonic/react4xp/dist' }//,
 				//{ find: '/lib/xp/io', replacement: '/lib/xp/io' } // [!] Error: Could not load /lib/xp/io (imported by src/main/resources/lib/enonic/react4xp/dependencies.ts): ENOENT: no such file or directory, open '/lib/xp/io'
 			]
 		}),
-		/*nodeResolve({
-			resolveOnly: [ // Do not bundle these:
-				//'@enonic/js-utils'
-				'/lib/xp/io'
-			]
-		}),*/
+		//nodeResolve(),
 		typescript(),
 		commonjs(),
 	]
