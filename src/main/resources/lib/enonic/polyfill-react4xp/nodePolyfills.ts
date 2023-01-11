c85ed7c153bbffc04c84af46e11aea0186cfdaa6
Christian Westgaard <comlock@gmail.com>
2023-01-11 14:21:02 +0100

    Started on Polyfill setTimeout #650

--- src/main/resources/lib/enonic/polyfill-react4xp/nodePolyfills.ts
+++ src/main/resources/lib/enonic/polyfill-react4xp/nodePolyfills.ts
@@ -1,9 +1,60 @@
-//──────────────────────────────────────────────────────────────────────────────
-// text-encoding
-//──────────────────────────────────────────────────────────────────────────────
 import {TextEncoder} from 'text-encoding';
 
-(function (context) {
-    if (typeof context.TextEncoder === 'undefined') context.TextEncoder = TextEncoder;
-})((1, eval)('this'));
 
+type Brand<
+    Base,
+    Branding
+> = Base & {
+  '__type__': Branding
+};
+
+// The returntype for window.setTimeout (in the browser) is number
+type TimerHandle = Brand<number,'TimerHandle'>
+
+
+(function (context) {
+    //──────────────────────────────────────────────────────────────────────────
+    // When doing SSR it makes no sense to do anything asyncronously.
+    // Therefore these should just be dummies:
+    // * clearInterval
+    // * clearTimeout
+    // * queueMicrotask
+    // * setInterval
+    // * setTimeout
+    //──────────────────────────────────────────────────────────────────────────
+    if (typeof context.clearInterval === 'undefined') {
+        context.clearInterval = (handle: TimerHandle) => undefined
+    }
+    if (typeof context.clearTimeout === 'undefined') {
+        context.clearTimeout = (handle: TimerHandle) => undefined
+    }
+    if (typeof context.queueMicrotask === 'undefined') {
+        context.queueMicrotask = (
+            callback: () => void
+        ) => {
+            return undefined as void;
+        };
+    }
+    if (typeof context.setInterval === 'undefined') {
+        context.setInterval = (
+            callback: (args: void) => void,
+            delay?: number
+        ) => {
+            return 0 as TimerHandle;
+        };
+    }
+    if (typeof context.setTimeout === 'undefined') {
+        context.setTimeout = (
+            callback: (args: void) => void,
+            delay?: number
+        ) => {
+            return 0 as TimerHandle;
+        };
+    }
+    //──────────────────────────────────────────────────────────────────────────
+    // text-encoding
+    //──────────────────────────────────────────────────────────────────────────
+    if (typeof context.TextEncoder === 'undefined') {
+        context.TextEncoder = TextEncoder;
+    }
+})((1, eval)('this'));
