9cc5f5a8afa4f6e821be2da85752e611fe46cea3
rymsha <rymsha@yandex.ru>
2023-03-15 15:10:47 +0100

    Don't define document in global namespace #738

--- src/main/resources/lib/enonic/polyfill-react4xp/polyfillBasics.js
+++ src/main/resources/lib/enonic/polyfill-react4xp/polyfillBasics.js
@@ -4,5 +4,4 @@ if (typeof global === 'undefined') { var global = this; }
 if (typeof exports === 'undefined') { var exports = {}; }
 if (typeof process === 'undefined') { var process = {env:{}}; }
 if (typeof console === 'undefined') { var console = {debug : print, log: print, warn: print, error: print}; }
-if (typeof document === 'undefined') { var document = {}; }
 if (typeof window.addEventListener !== 'function') { window.addEventListener = function () {}; }
