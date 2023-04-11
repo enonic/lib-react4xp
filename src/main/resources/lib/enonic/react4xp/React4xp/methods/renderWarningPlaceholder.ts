1594009981fc70d7cd93ee37cd1950371e4d301b
Christian Westgaard <comlock@gmail.com>
2023-04-11 17:41:13 +0200

    Fix #780 When ssr:false don't even try SSR

---
+++ src/main/resources/lib/enonic/react4xp/React4xp/methods/renderWarningPlaceholder.ts
@@ -0,0 +1,21 @@
+import type { React4xp } from '/types';
+
+
+export function renderWarningPlaceholder(this: React4xp) {
+	const {jsxPath, react4xpId} = this;
+	return `<div class="react4xp-warning">
+	<style>
+		.react4xp-warning {
+			background-color:#ffffb6;
+			border: 1px solid #8b8b00;
+			color: #000;
+			font-family:monospace;
+			font-size:12px;
+			padding:1em;
+		}
+	</style>
+	<p>
+		This React4xp component "<strong>${jsxPath}</strong>" (with id <strong>${react4xpId}</strong>) is configured to be rendered client-side and is thus disabled in Content Studio edit mode.
+	</p>
+</div>`;
+}
