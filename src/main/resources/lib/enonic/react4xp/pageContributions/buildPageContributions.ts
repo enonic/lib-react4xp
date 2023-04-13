e0aabf2ea8a4f2df5750b1e2adf9e132bd8fe559
Christian Westgaard <comlock@gmail.com>
2023-04-13 09:16:37 +0200

    Fix #49 W3C validator errors
    INFO: Trailing slash on void elements has no effect
    https://github.com/validator/validator/wiki/Markup-%C2%BB-Void-elements#trailing-slashes-in-void-element-start-tags-do-not-mark-the-start-tags-as-self-closing
    
    and interacts badly with unquoted attribute values.
    https://github.com/validator/validator/wiki/Markup-%C2%BB-Void-elements#trailing-slashes-directly-preceded-by-unquoted-attribute-values
    
    Example is without a trailing slash:
    https://www.w3schools.com/tags/tag_link.asp

--- src/main/resources/lib/enonic/react4xp/pageContributions/buildPageContributions.ts
+++ src/main/resources/lib/enonic/react4xp/pageContributions/buildPageContributions.ts
@@ -42,7 +42,7 @@ export function buildPageContributions({
 	for (let i = 0; i < componentChunkUrls.length; i++) {
 		const componentChunkUrl = componentChunkUrls[i];
 		if (endsWith(componentChunkUrl, '.css')) {
-			pageContributions.headEnd.push(`<link href="${componentChunkUrl}" rel="stylesheet" type="text/css" />\n`);
+			pageContributions.headEnd.push(`<link href="${componentChunkUrl}" rel="stylesheet" type="text/css">\n`);
 		} else if(!suppressJS) { // Treat other dependencies as JS and add them in a script tag. Unless suppressJS, in which case: skip them.
 			pageContributions.headEnd.push(`<script defer src="${componentChunkUrl}"></script>\n`);
 		}
