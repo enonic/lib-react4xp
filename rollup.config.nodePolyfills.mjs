0ede141824ce8edf32dacd4921e3535516216188
Christian Westgaard <comlock@gmail.com>
2023-04-13 10:15:47 +0200

    Fix #777 resolve build time so require dissappears from target bundle

--- rollup.config.nodePolyfills.mjs
+++ rollup.config.nodePolyfills.mjs
@@ -5,26 +5,26 @@ import typescript from '@rollup/plugin-typescript';
 const outDir = 'build/resources/main/lib/enonic/polyfill-react4xp';
 
 export default {
-    input: 'src/main/resources/lib/enonic/polyfill-react4xp/nodePolyfills.ts',
-    output: {
-        dir: outDir,
+	input: 'src/main/resources/lib/enonic/polyfill-react4xp/nodePolyfills.ts',
+	output: {
+		dir: outDir,
 
-        format: 'cjs',
+		format: 'cjs',
 
-        preserveModules: false, // We want to bundle everything into one bundle, so this should be false!
-    },
-    plugins: [
+		preserveModules: false, // We want to bundle everything into one bundle, so this should be false!
+	},
+	plugins: [
 		alias({
 			entries: [
-				{ find: 'text-encoding', replacement: './node_modules/text-encoding/index.js' },
+				{ find: '@sinonjs/text-encoding', replacement: './node_modules/@sinonjs/text-encoding/index.js' },
 			]
 		}),
-        typescript({
-            compilerOptions: {
-                outDir,
-                target: 'es5'
-            }
-        }),
-        commonjs()
-    ]
+		typescript({
+			compilerOptions: {
+				outDir,
+				target: 'es5'
+			}
+		}),
+		commonjs()
+	]
 };
