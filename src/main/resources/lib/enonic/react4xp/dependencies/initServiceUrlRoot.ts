961cc9e780659fa6c05ca0fb44b1823015808050
Christian Westgaard <comlock@gmail.com>
2023-09-15 13:58:15 +0200

    1011 initServiceUrlRoot(servicename, { type: 'absolute' })

--- src/main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts
+++ src/main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts
@@ -1,16 +1,23 @@
-import {assetUrl as getAssetUrl} from '/lib/xp/portal';
+import { assetUrl as getAssetUrl } from '/lib/xp/portal';
 
 /*
 * Asseturl should work in any context.
 * Hack until lib-static generates perfect static asset urls.
 */
-export function initServiceUrlRoot(serviceName = '') {
-	const assetUrl = getAssetUrl({path:'/'});
-	//log.debug('initServiceUrlRoot(%s) assetUrl:%s', serviceName, assetUrl);
+export function initServiceUrlRoot(serviceName = '', {
+	type = 'server'
+}: {
+	type?: 'server' | 'absolute'
+} = {}) {
+	const assetUrl = getAssetUrl({
+		path:'/',
+		type
+	});
+	// log.debug('initServiceUrlRoot(%s) assetUrl:%s', serviceName, assetUrl);
 	const serviceUrlRoot = assetUrl
-        .replace(/\/edit\/([^\/]+)\/([^\/]+)\/_\/asset/,'/preview/$1/$2/_/asset') // Fix BUG Assets give 404 in edit mode #476
+		.replace(/\/edit\/([^\/]+)\/([^\/]+)\/_\/asset/,'/preview/$1/$2/_/asset') // Fix BUG Assets give 404 in edit mode #476
 		.replace(/\/_\/asset\/.*$/, `/_/service/${app.name}/${serviceName}/`)
-		.replace(/\/{2,}/g, '/');
-	//log.debug('initServiceUrlRoot(%s) -> %s', serviceName, serviceUrlRoot);
+		.replace(/\/{2,}$/, '/');
+	// log.debug('initServiceUrlRoot(%s) -> %s', serviceName, serviceUrlRoot);
 	return serviceUrlRoot;
 }
