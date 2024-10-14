e8bed440dc11aa515bc81e0741dd0b16257261c0
Christian Westgaard <comlock@gmail.com>
2024-10-14 12:47:25 +0200

    Use lib-static 2.0.0-B5

--- src/main/resources/lib/enonic/react4xp/asset/immutableGetter.ts
+++ src/main/resources/lib/enonic/react4xp/asset/immutableGetter.ts
@@ -1,17 +1,10 @@
-import type {
-	Request,
-	Response
-} from '../../../..';
-
-
 import {R4X_TARGETSUBDIR} from '@enonic/react4xp';
-// @ts-ignore
 import {requestHandler} from '/lib/enonic/static';
 
 
-export const immutableGetter = (request: Request): Response => requestHandler(request,{
+export const immutableGetter = (request) => requestHandler(request,{
 	etag: false, // default is true in production and false in development
-	relativePath: (request :Request) => {
+	relativePath: (request) => {
 		const prefix = request.contextPath;
 		return prefix ? request.rawPath.substring(prefix.length) : request.rawPath;
 	},
