5de9eda7d6af66302d10012c9c9c5daee4d84daa
Christian Westgaard <comlock@gmail.com>
2024-10-18 13:48:50 +0200

    Fix #1467 Handle immutable fonts

--- src/main/resources/types/Stats.d.ts
+++ src/main/resources/types/Stats.d.ts
@@ -1,31 +1,40 @@
 export interface Stats {
-	outputPath: string
+	// outputPath: string
 	assetsByChunkName: Record<string, string[]>
+	assets: {
+		info: {
+			sourceFilename?: string
+			immutable?: boolean
+			javascriptModule?: boolean
+			contenthash?: string
+		}
+		name: string
+	}[]
 	entrypoints: {
 		[entry: string]: {
-			name: string
+			// name: string
 
 			// https://webpack.js.org/configuration/optimization/#optimizationchunkids
 			// deterministic: Short numeric ids which will not be changing between compilation. Good for long term caching. Enabled by default for production mode.
 			// named: Readable ids for better debugging.
 			// In Webpack 4 the default was named
 			// In Webpack 5 the default is deterministic
-			chunks: (number|string)[]
+			// chunks: (number|string)[]
 
 			assets: {
 				name: string
-				size?: number
+				// size?: number
 			}[]
-			filteredAssets: number
-			assetsSize: number|null
+			// filteredAssets: number
+			// assetsSize: number|null
 			auxiliaryAssets: {
 				name: string
-				size?: number
+				// size?: number
 			}[]
-			filteredAuxiliaryAssets: number
-			auxiliaryAssetsSize: number|null
-			children: object
-			childAssets: object
+			// filteredAuxiliaryAssets: number
+			// auxiliaryAssetsSize: number|null
+			// children: object
+			// childAssets: object
 		}
 	}
 	errors: unknown[]
