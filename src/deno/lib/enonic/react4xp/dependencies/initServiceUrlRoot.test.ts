62c3200d69acb8b679df50b1282bd192060e7224
Christian Westgaard <comlock@gmail.com>
2024-10-18 14:46:41 +0200

    Try more awaits

--- src/deno/lib/enonic/react4xp/dependencies/initServiceUrlRoot.test.ts
+++ src/deno/lib/enonic/react4xp/dependencies/initServiceUrlRoot.test.ts
@@ -6,74 +6,66 @@ declare namespace globalThis {
 	let app: App
 	let log: Log
 	let __: DoubleUnderscore
-	// let _devMode: boolean
 	let _assetUrl: string
-	// let _resources: Record<string, {
-	// 	bytes?: string
-	// 	exists?: boolean
-	// }>
 }
 
-await Deno.test("handles mode: inline", () => {
-	globalThis._assetUrl = '/admin/site/inline/default/draft/_/asset/com.enonic.app.react4xp:0000018a9816e580';
-	import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts').then(({initServiceUrlRoot}) => {
-		assertEquals(initServiceUrlRoot(), '/admin/site/inline/default/draft/_/service/com.enonic.app.react4xp/');
-	});
+function mockAssetUrl(mode: string) {
+	return `/admin/site/${mode}/default/draft/_/asset/com.enonic.app.react4xp:0000018a9816e580`;
+}
+
+await Deno.test("handles mode: inline", async () => {
+	globalThis._assetUrl = mockAssetUrl('inline');
+	const {initServiceUrlRoot} = await import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts');
+	assertEquals(initServiceUrlRoot(), '/admin/site/inline/default/draft/_/service/com.enonic.app.react4xp/');
 });
 
-await Deno.test("handles mode: inline using type: absolute", () => {
-	globalThis._assetUrl = '/admin/site/inline/default/draft/_/asset/com.enonic.app.react4xp:0000018a9816e580';
-	import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts').then(({initServiceUrlRoot}) => {
-		assertEquals(initServiceUrlRoot({
-			urlType: 'absolute'
-		}), 'http://localhost:8080/admin/site/inline/default/draft/_/service/com.enonic.app.react4xp/');
-	});
+await Deno.test("handles mode: inline using type: absolute", async () => {
+	globalThis._assetUrl = mockAssetUrl('inline');
+	const {initServiceUrlRoot} = await import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts');
+	assertEquals(initServiceUrlRoot({
+		urlType: 'absolute'
+	}), 'http://localhost:8080/admin/site/inline/default/draft/_/service/com.enonic.app.react4xp/');
 });
 
-await Deno.test("handles mode: edit", () => {
-	globalThis._assetUrl = '/admin/site/edit/default/draft/_/asset/com.enonic.app.react4xp:0000018a9816e580';
+await Deno.test("handles mode: edit", async () => {
+	globalThis._assetUrl = mockAssetUrl('edit');
+	const {initServiceUrlRoot} = await import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts');
+	assertEquals(initServiceUrlRoot(), '/admin/site/preview/default/draft/_/service/com.enonic.app.react4xp/');
+});
+
+await Deno.test("handles mode: edit type: absolute", async () => {
+	globalThis._assetUrl = mockAssetUrl('edit');
+	const {initServiceUrlRoot} = await import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts');
+	assertEquals(initServiceUrlRoot({
+		urlType: 'absolute'
+	}), 'http://localhost:8080/admin/site/preview/default/draft/_/service/com.enonic.app.react4xp/');
+});
+
+await Deno.test("handles mode: preview", async () => {
+	globalThis._assetUrl = mockAssetUrl('preview');
 	import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts').then(({initServiceUrlRoot}) => {
 		assertEquals(initServiceUrlRoot(), '/admin/site/preview/default/draft/_/service/com.enonic.app.react4xp/');
 	});
 });
 
-await Deno.test("handles mode: edit type: absolute", () => {
-	globalThis._assetUrl = '/admin/site/edit/default/draft/_/asset/com.enonic.app.react4xp:0000018a9816e580';
-	import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts').then(({initServiceUrlRoot}) => {
-		assertEquals(initServiceUrlRoot({
-			urlType: 'absolute'
-		}), 'http://localhost:8080/admin/site/preview/default/draft/_/service/com.enonic.app.react4xp/');
-	});
+await Deno.test("handles mode: preview type: absolute", async () => {
+	globalThis._assetUrl = mockAssetUrl('preview');
+	const {initServiceUrlRoot} = await import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts');
+	assertEquals(initServiceUrlRoot({
+		urlType: 'absolute'
+	}), 'http://localhost:8080/admin/site/preview/default/draft/_/service/com.enonic.app.react4xp/');
 });
 
-await Deno.test("handles mode: preview", () => {
-	globalThis._assetUrl = '/admin/site/preview/default/draft/_/asset/com.enonic.app.react4xp:0000018a9816e580';
-	import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts').then(({initServiceUrlRoot}) => {
-		assertEquals(initServiceUrlRoot(), '/admin/site/preview/default/draft/_/service/com.enonic.app.react4xp/');
-	});
-});
-
-await Deno.test("handles mode: preview type: absolute", () => {
-	globalThis._assetUrl = '/admin/site/preview/default/draft/_/asset/com.enonic.app.react4xp:0000018a9816e580';
-	import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts').then(({initServiceUrlRoot}) => {
-		assertEquals(initServiceUrlRoot({
-			urlType: 'absolute'
-		}), 'http://localhost:8080/admin/site/preview/default/draft/_/service/com.enonic.app.react4xp/');
-	});
-});
-
-await Deno.test("handles mode: live", () => {
+await Deno.test("handles mode: live", async () => {
 	globalThis._assetUrl = '/site/default/master/_/asset/com.enonic.app.react4xp:0000018a9816e580';
-	import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts').then(({initServiceUrlRoot}) => {
-		assertEquals(initServiceUrlRoot(), '/site/default/master/_/service/com.enonic.app.react4xp/');
-	});
+	const {initServiceUrlRoot} = await import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts');
+	assertEquals(initServiceUrlRoot(), '/site/default/master/_/service/com.enonic.app.react4xp/');
 });
 
-await Deno.test("handles mode: live type: absolute", () => {
+await Deno.test("handles mode: live type: absolute", async () => {
 	globalThis._assetUrl = '/site/default/master/_/asset/com.enonic.app.react4xp:0000018a9816e580';
-	import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts').then(({initServiceUrlRoot}) => {
-		assertEquals(initServiceUrlRoot({
-			urlType: 'absolute'
-		}), 'http://localhost:8080/site/default/master/_/service/com.enonic.app.react4xp/');
-	});
+	const {initServiceUrlRoot} = await import('../../../../../main/resources/lib/enonic/react4xp/dependencies/initServiceUrlRoot.ts');
+	assertEquals(initServiceUrlRoot({
+		urlType: 'absolute'
+	}), 'http://localhost:8080/site/default/master/_/service/com.enonic.app.react4xp/');
 });
