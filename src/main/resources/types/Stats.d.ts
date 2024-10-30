922afb414a93fa52c047e2434b17c72565d068cd
Christian Westgaard <comlock@gmail.com>
2024-10-30 14:55:30 +0100

    cleanup

--- src/main/resources/types/Stats.d.ts
+++ src/main/resources/types/Stats.d.ts
@@ -1,44 +1,23 @@
 export interface Stats {
-	// outputPath: string
 	assetsByChunkName: Record<string, string[]>
 	assets: {
 		info: {
-			sourceFilename?: string
-			immutable?: boolean
-			javascriptModule?: boolean
-			contenthash?: string
+			immutable?: boolean // This is the only one that's kept when doing StatsWriterPlugin.transform.
 		}
 		name: string
 	}[]
 	entrypoints: {
 		[entry: string]: {
-			// name: string
-
-			// https://webpack.js.org/configuration/optimization/#optimizationchunkids
-			// deterministic: Short numeric ids which will not be changing between compilation. Good for long term caching. Enabled by default for production mode.
-			// named: Readable ids for better debugging.
-			// In Webpack 4 the default was named
-			// In Webpack 5 the default is deterministic
-			// chunks: (number|string)[]
-
 			assets: {
 				name: string
-				// size?: number
 			}[]
-			// filteredAssets: number
-			// assetsSize: number|null
 			auxiliaryAssets: {
 				name: string
-				// size?: number
 			}[]
-			// filteredAuxiliaryAssets: number
-			// auxiliaryAssetsSize: number|null
-			// children: object
-			// childAssets: object
 		}
 	}
 	errors: unknown[]
-	errorsCount: number
+	errorsCount?: number // rspack removes this (in the next PR)
 	warnings: unknown[]
-	warningsCount: number
+	warningsCount?: number // rspack removes this (in the next PR)
 }
