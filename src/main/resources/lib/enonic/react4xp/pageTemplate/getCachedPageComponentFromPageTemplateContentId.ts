3d32bf4157a9ffdd7cf70b95c8bb776bb19e9fac
Christian Westgaard <comlock@gmail.com>
2024-11-25 11:35:06 +0100

    wip #1500 Handle page templates with caching

---
+++ src/main/resources/lib/enonic/react4xp/pageTemplate/getCachedPageComponentFromPageTemplateContentId.ts
@@ -0,0 +1,24 @@
+import type { PageComponent } from '@enonic-types/core';
+import type { PageTemplateContent } from '/types';
+
+import {get as getContentByKey} from '/lib/xp/content';
+import { pageTemplateContentIdToPageComponentCache } from '/lib/enonic/react4xp/pageTemplate/pageTemplateContentIdToPageComponentCache';
+
+
+
+export const getCachedPageComponentFromPageTemplateContentId = ({
+	pageTemplateContentId
+}: {
+	pageTemplateContentId: string
+}): PageComponent => pageTemplateContentIdToPageComponentCache.get(pageTemplateContentId, () => {
+	const content = getContentByKey<PageTemplateContent>({
+		key: pageTemplateContentId,
+	});
+	if (!content) {
+		throw new Error(`Unable to find a page template for content id '${pageTemplateContentId}'!`);
+	}
+	if (content.type !== 'portal:page-template') {
+		throw new Error(`Content not a page template! Content ID:'${pageTemplateContentId}'`);
+	}
+	return content.page;
+});
