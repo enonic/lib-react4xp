5b04cd7a4f05d71effae33a08ae099d35a7632aa
anatol-sialitski <53557255+anatol-sialitski@users.noreply.github.com>
2026-06-17 13:19:52 +0200

    Remove service url builder #2151 (#2201)
    * Remove service url builder #2151
    
    * Bump @enonic/react4xp
    
    ---------
    
    Co-authored-by: alansemenov <ase@enonic.com>

--- src/main/resources/lib/enonic/react4xp/dependencies/stripSlashes.ts
+++
@@ -1,7 +0,0 @@
-const slashesAtBeginning = /^\/+/;
-const slashesAtEnd = /\/+$/;
-
-
-export function stripSlashes(suffix :string) {
-    return suffix.replace(slashesAtBeginning, '').replace(slashesAtEnd, '');
-};
